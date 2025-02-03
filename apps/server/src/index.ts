dfb318248d6ed0a48f2b5afcd3fc25aaea077d1d
romanticxdraculla <romanticdraculla@gmail.com>
2025-02-03 15:04:50 +0545

    feat: update ui settings, add better-auth

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,15 @@
 import { Hono } from 'hono'
+import { auth } from './lib/auth';
 
 const app = new Hono()
+
+app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+export default {
+  port: 8787,
+  fetch: app.fetch
+}
