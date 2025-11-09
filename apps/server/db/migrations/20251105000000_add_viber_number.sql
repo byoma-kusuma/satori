0cad4e650ded1c0d87e6d0098a971ced113ce3df
Misa Yednap <misa.yednap@gmail.com>
2025-11-09 04:11:28 -0500

    issue importing, viber number issue

--- apps/server/db/migrations/20251105000000_add_viber_number.sql
+++ apps/server/db/migrations/20251105000000_add_viber_number.sql
@@ -1,5 +1,7 @@
 -- migrate:up
 ALTER TABLE person ADD COLUMN viber_number VARCHAR(50);
+ALTER TABLE registration ADD COLUMN viber_number VARCHAR(40);
 
 -- migrate:down
+ALTER TABLE registration DROP COLUMN viber_number;
 ALTER TABLE person DROP COLUMN viber_number;
